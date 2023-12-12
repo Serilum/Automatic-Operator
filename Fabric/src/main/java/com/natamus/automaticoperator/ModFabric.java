efddeb963d4542b9f5c73989ab4b583f736b4362
Rick South <rick@natamus.com>
2023-12-12 11:02:18 +0100

    Automatic Operator source code for 1.20.4.

---
+++ Fabric/src/main/java/com/natamus/automaticoperator/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.automaticoperator;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.automaticoperator.events.WorldJoinEvent;
+import com.natamus.automaticoperator.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_LOGGED_IN.register((Level world, Player player) -> {
+			WorldJoinEvent.onPlayerLoggedIn(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
