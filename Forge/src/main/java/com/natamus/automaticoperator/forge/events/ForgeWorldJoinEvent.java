170ed9e0c59b316753c7530d500762f44a2f3811
Rick South <rick@natamus.com>
2023-12-12 11:01:57 +0100

    Automatic Operator source code for 1.19.4.

---
+++ Forge/src/main/java/com/natamus/automaticoperator/forge/events/ForgeWorldJoinEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.automaticoperator.forge.events;
+
+import com.natamus.automaticoperator.events.WorldJoinEvent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeWorldJoinEvent {
+	@SubscribeEvent()
+	public void onSpawn(PlayerEvent.PlayerLoggedInEvent e) {
+		Player player = e.getEntity();
+		WorldJoinEvent.onPlayerLoggedIn(player.getLevel(), player);
+	}
+}
