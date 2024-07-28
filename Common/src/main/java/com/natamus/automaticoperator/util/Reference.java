0dc852635c2018ab2a001ec2054d2817d7c708c1
Rick South <rick@natamus.com>
2024-07-28 07:43:36 +0200

    (v1.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/automaticoperator/util/Reference.java
+++ Common/src/main/java/com/natamus/automaticoperator/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.automaticoperator.util;
 public class Reference {
 	public static final String MOD_ID = "automaticoperator";
 	public static final String NAME = "Automatic Operator";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
