cee238a034e7efc3a4c70de58bcf7d92774bf3e7
Rick South <rick@natamus.com>
2025-07-18 03:13:29 +0200

    Automatic Operator source code for 1.21.8.

---
+++ Common/src/main/java/com/natamus/automaticoperator/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.automaticoperator;
+
+import com.natamus.automaticoperator.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
