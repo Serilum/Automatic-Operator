d5c3908390e32e29ab3f6894b843bb84d8ec483d
Rick South <rick@natamus.com>
2025-12-11 02:58:26 +0100

    Automatic Operator source code for 1.21.11.

---
+++ NeoForge/src/main/java/com/natamus/automaticoperator/neoforge/events/NeoForgeWorldJoinEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.automaticoperator.neoforge.events;
+
+import com.natamus.automaticoperator.events.WorldJoinEvent;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeWorldJoinEvent {
+	@SubscribeEvent()
+	public static void onSpawn(PlayerEvent.PlayerLoggedInEvent e) {
+		Player player = e.getEntity();
+		WorldJoinEvent.onPlayerLoggedIn(player.level(), player);
+	}
+}
